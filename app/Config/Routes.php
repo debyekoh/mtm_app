be19c91d663b2034472100a15c3b264e53b084be
debyekoh <83970754+debyekoh@users.noreply.github.com>
2023-09-06 07:31:12 +0700

    MTM

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
+
+
+$routes->get('/dmf_dashboards', 'DiesManufacturingController\Dashboards::index');
+$routes->get('/dmf_assy_set', 'DiesManufacturingController\AssySet::index');
+$routes->get('/dmf_part', 'DiesManufacturingController\Part::index');
+$routes->get('/dmf_consumable', 'DiesManufacturingController\Consumable::index');
+$routes->get('/dmf_create_new_standar', 'DiesManufacturingController\Create::index');
